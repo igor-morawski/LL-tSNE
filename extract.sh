3b3d75fff6eb0a5d429bad6d59e5ca83859695bf
Igor Morawski <igor@cml23.csie.ntu.edu.tw>
2021-03-23 09:48:57 +0800

    run extract

--- extract.sh
+++ extract.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 cp extract.py ~/mmdetection/tools/.
 GPU_NUM=2 # ${GPU_NUM}
-CONFIG_FILE=/auto/phd/09/igor/mmdetection/configs/i2102/210218JPG_MM4G1_Pretrained_UNetResNet50.py
+# CONFIG_FILE=/auto/phd/09/igor/mmdetection/configs/i2102/210218JPG_MM4G1_Pretrained_UNetResNet50.py
+# CHECKPOINT=/tmp2/igor/checkpoints/PUR50_epoch_107.pth
+CONFIG_FILE=/auto/phd/09/igor/mmdetection/configs/i2102/210218JPG_MM4G1_ResNet50.py
 CHECKPOINT=/tmp2/igor/checkpoints/PUR50_epoch_107.pth
 # ${CONFIG_FILE}
 EXPERIMENT_DIR=$(pwd)
@@ -9,4 +11,4 @@ pwd
 echo "Navigating to mmdet..."
 cd ~/mmdetection/tools
 pwd
-python extract.py ${CONFIG_FILE} ${CHECKPOINT} --output_dir=${EXPERIMENT_DIR} --dataset_dir=/tmp2/igor/LL-tSNE/resized
+CUDA_VISIBLE_DEVICES=0 python extract.py ${CONFIG_FILE} ${CHECKPOINT} --output_dir=${EXPERIMENT_DIR}/features --dataset_dir=/tmp2/igor/LL-tSNE/resized 
